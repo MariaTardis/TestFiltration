5a893a16563acfc56bb04673a3d9e1d76447323b
MariaTardis <torvintardis@gmail.com>
2016-01-15 19:05:41 +0600

    two test case

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using selenium_third.src;
+using OpenQA.Selenium.Support.UI;
+
+namespace selenium_third {
+	class Program {
+		static void Main(string[] args) {}		
+
+		[SetUp]
+		public void Initialize() {
+			PropertiesCollection.driver  = new ChromeDriver();
+			Console.WriteLine("Start test");
+			PropertiesCollection.driver.Navigate().GoToUrl("http://2gis.ru/countries/global");
+			Console.WriteLine("Go to http://2gis.ru/countries/global");
+		}
+
+		[Test]
+		public void ExecuteTestInputExistentTown() {
+			IWebElement town = PropertiesCollection.driver.FindElement(By.ClassName("world__searchInput"));
+			Console.WriteLine("Select the line with -Input the name of the town-");
+			town.SendKeys("Новосибирск");
+			Console.WriteLine("Input in the line -Новосибирск- ");
+		}
+
+		[Test]
+		public void ExecuteTestInputExistentTownByLatin() {
+			IWebElement town = PropertiesCollection.driver.FindElement(By.ClassName("world__searchInput"));
+			Console.WriteLine("Select the line with -Input the name of the town-");
+			town.SendKeys("Vjcrdf");
+			Console.WriteLine("Input in the line -Vjcrdf-");
+		}
+
+		[TearDown]
+		public void CleanUp() {
+			PropertiesCollection.driver.Close();
+			Console.WriteLine("End test");
+		}
+	}
+}
---
+++ SeleniumFirst/src/PropertiesCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace selenium_third.src {
+
+	enum PropertyType { Id, Name, ClassName }
+
+	class PropertiesCollection {
+
+		public static IWebDriver driver { get; set; }
+	}
+}
